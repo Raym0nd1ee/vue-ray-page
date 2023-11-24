330afa50ee9262f647bc56f902411b8426ecd369
Raymond EBLee <96124950+eblee1234@users.noreply.github.com>
2023-11-24 16:04:46 +0800

    add dist

---
+++ dist/assets/AboutView-c3e11642.js
@@ -0,0 +1,1 @@
+import{_ as t,o,c as s,a as e}from"./index-78a57dfb.js";const a={},_={class:"about"},n=e("h1",null,"This is an about page",-1),c=e("p",null,"2nd Commit add this paragraph to about page",-1),i=[n,c];function r(d,u){return o(),s("div",_,i)}const p=t(a,[["render",r]]);export{p as default};
