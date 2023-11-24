a8267e8cd7303099518fbc648e5474c508eefeb0
eblee1234 <leeengbee@yahoo.com>
2023-11-24 15:28:10 +0800

    2nd commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <p>2nd Commit add this paragraph</p>
+    <p>2nd Commit add this paragraph to about page</p>
   </div>
 </template>
 
